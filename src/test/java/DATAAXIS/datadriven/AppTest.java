0fa46e6eba99076db0ff24eedb7c1e4cebb4c6d6
Vijaykrishnagundavarapu <vijaychowdary040@gmail.com>
2024-02-27 15:45:36 +0530

    first commit

---
+++ src/test/java/DATAAXIS/datadriven/AppTest.java
@@ -0,0 +1,20 @@
+package DATAAXIS.datadriven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
